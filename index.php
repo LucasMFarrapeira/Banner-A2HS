d8b407cd9e6777bce0be6c5be09aee905a640772
LucasMFarrapeira <lucas.alxen@gmail.com>
2017-11-14 12:10:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,56 @@
-<?php 
+<div class="skepost clearfix text-center">
+	<div class="page-container clearfix">
+		<div class='titlesec'>Más<span style='font-weight: 200;'> opciones</span></div>
+		<div style="margin-bottom: 25px;" class="con-key-feature span3 skt_animate_when_almost_visible small-to-large skt_start_animation">
+			<div class="con-icon-wrap">
+				<a href="http://clubpiraguismopenedo.com/index.php/multimedia/">
+					<div class="con-feature-icon">
+						<i class="fa fa-camera-retro"></i>
+						<div class="con-icon-title">Galería</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div style="margin-bottom: 25px;" class="con-key-feature span3 skt_animate_when_almost_visible small-to-large skt_start_animation">
+			<div class="con-icon-wrap">
+				<a href="http://clubpiraguismopenedo.com/index.php/categoria-producto/tienda/">
+					<div class="con-feature-icon">
+						<i class="fa  fa-shopping-cart"></i>
+						<div class="con-icon-title">Tienda</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div style="margin-bottom: 25px;" class="con-key-feature span3 skt_animate_when_almost_visible small-to-large skt_start_animation" id="calportada">
+			<div class="con-icon-wrap" style="padding:0px">
+				<a href="http://clubpiraguismopenedo.com/index.php/605-2/">
+					<div class="con-feature-icon" style="padding:0px">[event_calendar 604]</div>
+				</a>
+			</div>
+		</div>
+		<div style="margin-bottom: 25px;" class="con-key-feature span3 skt_animate_when_almost_visible small-to-large skt_start_animation">
+			<div class="con-icon-wrap">
+				<a href="http://clubpiraguismopenedo.com/index.php/revistas/">
+					<div class="con-feature-icon">
+						<i class="fa fa-book"></i>
+						<div class="con-icon-title">Revista</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div style="margin-bottom: 25px;" class="con-key-feature span3 skt_animate_when_almost_visible small-to-large skt_start_animation">
+			<div class="con-icon-wrap">
+				<a href="http://clubpiraguismopenedo.com/index.php/modalidades/">
+					<div class="con-feature-icon">
+						<i class="fa fa-list-ol"></i>
+						<div class="con-icon-title">Clasificaciones</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		
+	</div>
+</div>
 
-echo "asda";
 
-?>
+
